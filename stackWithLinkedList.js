8b10a38f05d03680617795ad1068e9249ecad208
luizamav <l.avetisyan7777@gmail.com>
2024-05-18 21:43:04 +0400

    created module using files

--- stackWithLinkedList.js
+++ stackWithLinkedList.js
@@ -1,3 +1,5 @@
+const LinkedList = require('./linkedListWithTailPointer.js')
+
 
 class Stack{
     constructor(){
@@ -30,96 +32,6 @@ class Stack{
 }
 
 
-class Node{
-    constructor(value){
-        this.next = null
-        this.value = value
-    }
-}
-
-class LinkedList{
-    constructor(){
-        this.head = null
-        this.tail = null
-        this.size = 0
-    }
-
-    isEmpty(){
-        return this.size === 0
-    }
-
-    getSize(){
-        return this.size
-    }
-
-    print(){
-        let element = this.head
-        while(element){
-            console.log(element.value)
-            element = element.next
-        }
-    }
-
-    prepend(value){
-        const node = new Node(value)
-        if(this.isEmpty()){
-            this.head = node
-            this.tail = node
-        }else{
-            node.next = this.head
-            this.head = node
-        }
-
-        ++this.size
-    }
-
-    append(value){
-        const node = new Node(value)
-        if(this.isEmpty()){
-            this.head = node
-            this.tail = node
-        } else{
-            this.tail.next = node
-            this.tail = node
-        }
-
-        ++this.size
-    }
-
-    removeFront(){
-        if(this.isEmpty()){
-            return null
-        }
-
-        const value = this.head.value
-        this.head = this.head.next
-        --this.size
-        return value
-    }
-
-    removeEnd(){
-        if(this.isEmpty()){
-            return null
-        }
-
-        const value = this.tail.value
-        if(this.size === 1){
-            this.head = null
-            this.tail = null
-        }else{
-            const element = this.head
-            while(element.next !== this.tail){
-                element = element.next
-            }
-
-            element.next = null
-            this.tail = element
-        }
-
-        --this.size
-        return value
-    }
-}
 
 
 // let stack = new Stack()
